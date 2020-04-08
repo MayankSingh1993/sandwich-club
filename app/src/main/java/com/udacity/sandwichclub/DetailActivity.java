d7e18432f6b5065710d6bdfa8075b6efa1330f51
Mayank Singh Chaudhary <singhchaudharymayank0@gmail.com>
2020-04-09 02:11:04 +0530

    parseSandwichJson & populateUi Updated.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,25 +2,42 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    @BindView(R.id.image_iv)
+    ImageView ingredientsIV;
+    @BindView(R.id.also_known_tv)
+    TextView alsoKnownAsTV;
+    @BindView(R.id.ingredients_tv)
+    TextView ingredientsTV;
+    @BindView(R.id.description_tv)
+    TextView description_TV;
+    @BindView(R.id.origin_tv)
+    TextView originTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -28,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
+        if (position == DEFAULT_POSITION)                {
             // EXTRA_POSITION not found in intent
             closeOnError();
             return;
@@ -43,10 +60,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(ingredientsIV);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +73,47 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+
+        //setting alsoKnownAs to  corresponding textview
+        List<String> alsoKnownAs=sandwich.getAlsoKnownAs();
+        if(!alsoKnownAs.isEmpty()){
+            for (String anotherName: alsoKnownAs
+            ) {
+                alsoKnownAsTV.append(anotherName);
+            }
+
+        }else {
+            alsoKnownAsTV.setText(R.string.not_available);
+        }
+
+        //setting ingredients to  corresponding textview
+        List<String> ingredients=sandwich.getAlsoKnownAs();
+        if(!ingredients.isEmpty()){
+            for (String ingredient: ingredients
+            ) {
+                ingredientsTV.append(ingredient);
+            }
+
+        }else {
+            ingredientsTV.setText(R.string.not_available);
+        }
+
+        if(!sandwich.getPlaceOfOrigin().isEmpty()){
+            //setting place of origin to corresponding textview
+            originTV.setText(sandwich.getPlaceOfOrigin());
+        }else {
+            originTV.setText(R.string.not_available);
+        }
+
+
+        if(!sandwich.getDescription().isEmpty()){
+            //setting description to  corresponding textview
+            description_TV.setText(sandwich.getDescription());
+        }else {
+            description_TV.setText(R.string.not_available);
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,57 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwichPojoObj =null;
+        try {
+            //root object
+           JSONObject sandwichRootObj = new JSONObject(json);
+           //name
+           JSONObject name = sandwichRootObj.optJSONObject("name");
+           //Extract  mainName string from name
+            String mainName = name.optString("mainName");
+
+
+            // List for storing another names of sandwich
+            List<String> alsoKnownAs = new ArrayList<>();
+            // Extract alsoKnownAs Array from name
+            JSONArray alsoKnownAsArray = name.optJSONArray("alsoKnownAs");
+
+           for(int i=0;i<alsoKnownAsArray.length();i++){
+               String anotherName = alsoKnownAsArray.optString(i);
+               alsoKnownAs.add(anotherName);
+
+           }
+           //Extract Place of origin
+            String placeOfOrigin = sandwichRootObj.optString("placeOfOrigin");
+            //Extract Description
+            String description = sandwichRootObj.optString("description");
+
+            //Extract image url
+            String imageurl = sandwichRootObj.optString("image");
+
+            // List for storing ingredients of sandwich
+            List<String> ingredients = new ArrayList<>();
+            // Extract ingredients Array from name
+            JSONArray ingredientsArray = sandwichRootObj.optJSONArray("ingredients");
+            for(int i=0;i<ingredientsArray.length();i++){
+                String ingredient = ingredientsArray.optString(i);
+                ingredients.add(ingredient);
+            }
+            sandwichPojoObj = new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,imageurl,ingredients);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sandwichPojoObj;
     }
 }
